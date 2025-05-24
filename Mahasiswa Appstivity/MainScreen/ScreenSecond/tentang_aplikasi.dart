b9e0b60a973c5351d5ec237ed462c0f0a20f8b20
JanSSH <jncodd7@gmail.com>
2025-05-24 16:08:18 +0700

    Update tentang_aplikasi.dart

--- Mahasiswa Appstivity/MainScreen/ScreenSecond/tentang_aplikasi.dart
+++ Mahasiswa Appstivity/MainScreen/ScreenSecond/tentang_aplikasi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mahasiswaappstivity/CustomWidget/nav_bar.dart';
 
 class TentangAplikasi extends StatefulWidget {
   const TentangAplikasi({super.key});
@@ -11,31 +12,70 @@ class _TentangAplikasiState extends State<TentangAplikasi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // AppBar
       appBar: AppBar(
         backgroundColor: Colors.white70,
-        title: Row(
-          children: [
-            Icon(Icons.person),
-            Text("Mahasiswa Aptivity"),
-          ],
+        centerTitle: true,
+        title: Text(
+          "Mahasiswa Appstivity",
+
+          // untuk desain style appbar
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
+      drawer: NavBar(),
+
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.person),
-          // Image.asset('assets/images/logo-mahasiswa-appstivity.png',),
+            // Image.asset('assets/images/logo-mahasiswa-appstivity.png',),
             //Durung iso Failed to load
-            SizedBox(height: 20,),
-            Text(
-                "Aplikasi sebagai fasilitas\nmonitoring dan pendukung\nkegiatan perkuliahan\nmahasiswa",
-              style: TextStyle(
+            SizedBox(height: 20),
+            Container(
+              // ukuran box teks bisa padding/margin
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              // untuk cek ukuran container aktifin warnanya
+              //color: Colors.blue,
+              child: Text(
+                softWrap: true,
+                textAlign: TextAlign.center,
+                "Aplikasi sebagai fasilitas monitoring dan pendukung kegiatan perkuliahan mahasiswa",
+                //ubah desain tulisan
+                style: TextStyle(
+                  fontSize: 16,
+                ),
               ),
-              textAlign: TextAlign.center,
             ),
+
             SizedBox(height: 20,),
-            Text("Contributor:",style: TextStyle(fontWeight: FontWeight.bold),),
-            Text("Januar Dwi Putra\nArif Akbarudin\nNurdin Mukti Widodo\nIrsyad Hasan\nMuhammad Syafii", style: TextStyle(),textAlign: TextAlign.center,)
+
+            Text(
+              "Contributor:",
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 4),
+            Text(
+              textAlign: TextAlign.center,
+              "Januar Dwi Putra\n"
+                  "Arif Akbarudin\n"
+                  "Nurdin Mukti Widodo\n"
+                  "Irsyad Hasan\n"
+                  "Muhammad Syafii",
+              //ubah desain tulisan contributor
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+
+
           ],
         ),
       ),
