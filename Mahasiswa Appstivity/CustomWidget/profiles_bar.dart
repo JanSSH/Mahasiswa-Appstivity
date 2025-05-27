1c9c6b3dc875258dcf44b85d9de56fc40d0d85ea
irsyad-hasan <230101053@mhs.udb.ac.id>
2025-05-27 17:39:41 +0700

    Update profiles_bar.dart

---
+++ Mahasiswa Appstivity/CustomWidget/profiles_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ProfilesBar extends StatefulWidget {
+  const ProfilesBar({super.key});
+
+  @override
+  State<ProfilesBar> createState() => _ProfilesBarState();
+}
+
+class _ProfilesBarState extends State<ProfilesBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          // Container Profil + Username + Tombol Sertifikat
+          Center(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey,
+                        ),
+                        child: const Icon(Icons.person, color: Colors.white),
+                      ),
+                      const SizedBox(width: 16),
+                      SizedBox(
+                        width: 200,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text('Icad'),
+                            Text(
+                              'Icad ni boss, jangan disenggol ya, elebew',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Sertifikat Mahasiswa',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
