ed55266fe08c06e694705f3e633e8c2c7f81073e
irsyad-hasan <230101053@mhs.udb.ac.id>
2025-05-27 17:47:06 +0700

    Update progress_bar.dart

--- Mahasiswa Appstivity/CustomWidget/progress_bar.dart
+++ Mahasiswa Appstivity/CustomWidget/progress_bar.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+class ProgressBar extends StatefulWidget {
+  const ProgressBar({super.key});
+
+  @override
+  State<ProgressBar> createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  double _progressValue = 0.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: const Text(
+                    'Progress Sertifikat:',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: LinearProgressIndicator(
+                    value: _progressValue,
+                    minHeight: 10,
+                    backgroundColor: Colors.grey[350],
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
